b17d5acf1fc2ce9f492760b1faef7ea7136efc31
eileenj14 <eileenj14@nycstudents.net>
2025-12-11 20:48:49 -0500

    Day 6 part 2 finished.

--- src/Day6.java
+++ src/Day6.java
@@ -6,16 +6,16 @@ import static java.lang.Integer.parseInt;
 public class Day6
 {
     public static int numOfProblems = 0;
-    public static int numOfInputsPerProblem = 0;
+    public static int numOfInputsPerProblem = 4;
     public static int[][] problemNums;
     public static String[] problemOps;
 
     public static void main(String[] args) throws FileNotFoundException
     {
         createHomework();
-        //System.out.println(getTotalOfAnswers());
+        System.out.println(getTotalOfAnswers());
         createHomework2();
-        //System.out.println(getTotalOfAnswers());
+        System.out.println(getTotalOfAnswers2());
     }
 
     public static long getTotalOfAnswers()
@@ -41,56 +41,77 @@ public class Day6
         String line = s.nextLine();
         String[] lineArray = line.split("\\s+");
         numOfProblems = lineArray.length;
-        numOfInputsPerProblem = 3;
         s = new Scanner(f);
         problemNums = new int[numOfProblems][numOfInputsPerProblem];
         problemOps = new String[numOfProblems];
-        int count = 0;
+        int i = 0;
         while(s.hasNextLine())
         {
             line = s.nextLine();
             lineArray = line.trim().split("\\s+");
-            numOfProblems = lineArray.length;
-            if(count < numOfInputsPerProblem)
+            if(i < numOfInputsPerProblem)
             {
-                for(int p = 0; p < numOfProblems; p++) problemNums[p][count] = parseInt(lineArray[p]);
-                count++;
+                for(int p = 0; p < numOfProblems; p++) problemNums[p][i] = parseInt(lineArray[p]);
+                i++;
             }
             else for(int p = 0; p < numOfProblems; p++) problemOps[p] = lineArray[p];
         }
+    }
+
+    public static long getTotalOfAnswers2()
+    {
+        long totalOfAnswers = 0;
+        for(int p = 0; p < numOfProblems; p++)
+        {
+            long answer = problemNums[p][0];
+            for(int i = 1; i < numOfInputsPerProblem; i++)
+            {
+                if(problemOps[numOfProblems - 1 - p].equals("*"))
+                {
+                    if(problemNums[p][i] != 0) answer *= problemNums[p][i];
+                }
+                else answer += problemNums[p][i];
+            }
+            totalOfAnswers += answer;
+        }
+        return totalOfAnswers;
     }
 
     public static void createHomework2() throws FileNotFoundException
     {
         File f = new File("Day6_Input.txt");
         Scanner s = new Scanner(f);
-        numOfInputsPerProblem = 4;
         problemNums = new int[numOfProblems][numOfInputsPerProblem];
-        String[] lines = new String[3];
-        for(int i = 0; i < 3; i++)
+        String[] lines = new String[numOfInputsPerProblem];
+        for(int i = 0; i < numOfInputsPerProblem; i++)
         {
             lines[i] = s.nextLine();
         }
-        int p = 0;
-        while(!lines[0].isEmpty())
+        int maxLength = 0;
+        for(int i = 0; i < numOfInputsPerProblem; i++)
         {
-            int i = 0;
-            int spaces = 0;
-            int num = 0;
-            for(int l = 0; l < 3; l++)
+            if(lines[i].length() > maxLength) maxLength = lines[i].length();
+        }
+        for(int i = 0; i < numOfInputsPerProblem; i++)
+        {
+            while(lines[i].length() != maxLength) lines[i] += " ";
+        }
+        for(int p = 0; p < numOfProblems; p++)
+        {
+            for(int i = 0; i < numOfInputsPerProblem + 1; i++)
             {
-                if(lines[l].charAt(0) == ' ')
+                String num = "";
+                for(int l = 0; l < numOfInputsPerProblem; l++)
                 {
-                    spaces++;
-                    lines[l] = lines[l].substring(1);
+                    if(lines[l].length() - 1 != -1)
+                    {
+                        num += lines[l].substring(lines[l].length() - 1);
+                        lines[l] = lines[l].substring(0, lines[l].length() - 1);
+                    }
                 }
-                else
-                {
-                    num = num * 10 + parseInt(lines[l].substring(0, 1));
-                    lines[l] = lines[l].substring(1);
-                }
+                if(num.isBlank()) break;
+                else problemNums[p][i] = parseInt(num.trim());
             }
         }
-        for(int x = 0; x < numOfInputsPerProblem; x++) System.out.println(problemNums[0][x]);
     }
 }
