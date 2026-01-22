6fdf95352c2113200f05ced5acbfd602b85da5f4
eileenj14 <eileenj14@nycstudents.net>
2026-01-21 22:02:56 -0500

    Day 10 part 1 started.

---
+++ src/Day10.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day10
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        File f = new File("Day10_Input.txt");
+        Scanner s = new Scanner(f);
+    }
+}
